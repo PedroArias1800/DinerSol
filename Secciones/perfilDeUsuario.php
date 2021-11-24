2d89bc1e06ff8e1e8ecb2c94ddb93e1acce5a681
JordanA20 <86572294+JordanA20@users.noreply.github.com>
2021-11-23 21:34:25 -0600

    Funcionalidades de perfilDeUsuario
    Se agregaron los procesos para editar perfil y cambiar contraseña y funciones js para verificar las contraseñas y algunos cambios en css de perfil.

--- Secciones/perfilDeUsuario.php
+++ Secciones/perfilDeUsuario.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -27,44 +25,83 @@
    
 
     <!-- Seccion -->
-     <div class="editar">
-    <div class="usuarios usuarios--editar">
-      <h2>Actualizar Usuario</h2>
+
+    <!--Editar Perfil-->
+    <div id="perfil" class="editar">
+      <div class="usuarios usuarios--editar">
+        <h2>Actualizar Usuario</h2>
+      </div>
+      <form action="../Procesos/actualizarPerfil.php" method="POST" enctype="multipart/form-data">
+        <div class="block-component">
+          <span class="outline">
+            <img class="outline-img" src="../Imagenes/FotosDePerfil/<?php echo  $datosDelUsuario->foto; ?>" />
+          </span>
+        </div>
+
+        <div class="user username">
+          <span class="icon uil uil-user"></span>
+          <input class="user  input-style" type="text" name="nombre" id="nombre" value="<?php echo  $datosDelUsuario->nombre; ?>" required>
+        </div>
+        <div class="user username">
+          <span class="icon uil uil-user"></span>
+          <input class=" user input-style" type="text" name="apellido" id="apellido" value="<?php echo $datosDelUsuario->apellido;  ?>" required>
+        </div>
+        <div class="user username">
+          <span class="icon uil uil-phone"></span>
+          <input class=" user input-style" type="text" name="telefono" id="telefono" value="<?php echo $datosDelUsuario->telefono;  ?>" required>
+        </div>
+        <div class="user disable username">
+          <span class="icon disable uil uil-envelope-edit"></span>
+          <input class="user disable  input-style" type="email" name="correo" id="correo" value="<?php echo $datosDelUsuario->email; ?>" required disabled>
+        </div>
+
+        <input name="ftpd" type="hidden" value="<?php echo  $datosDelUsuario->foto ?>"> </input>
+        <input name="foto" type="file" accept="image/*" />
+
+        <div class="submit">
+          <button type="submit">Actualizar</button>
+        </div>
+
+          <p class="presForm" onclick="cambiarForm(1)">Cambiar Contraseña</p>
+      </form>
     </div>
-    <form action="../php/actualizar.php" method="POST" enctype="multipart/form-data">
-      <div class="block-component">
-        <span class="outline">
-          <img class="outline-img" src="../Imagenes/<?php echo  $datosDelUsuario->foto; ?>" />
-        </span>
+
+    <!--Cambiar Contraseña-->
+    <div id="contraseña" class="editar invisible">
+      <div class="usuarios usuarios--editar">
+        <h2>Cambiar Contraseña</h2>
+        <p>Para cambiar la contraseña primero debe ingresar la contraseña actual seguido de la nueva contraseña.</p>
       </div>
+      <form action="../Procesos/actualizarContraseña.php" method="POST">
+        
+        <div class="user password">
+          <span class="icon uil uil-lock-open-alt"></span>
+          <input class="user  input-style" type="password" name="contraA" id="contraA" required placeholder="Contraseña actual">
+        </div>
+        <div class="container-password">
+          <p class="error" id="errorPass1"></p>
+          <div class="user password">
+            <span class="icon uil uil-lock-alt"></span>
+            <input class=" user input-style" type="password" name="contra1" id="contra1" required placeholder="Nueva contraseña">
+          </div>
+        </div>
+        
+        <div id="container-password">
+          <p class="error" id="errorPass2"></p>
+          <div class="user password">
+            <span class="icon uil uil-lock-alt"></span>
+            <input class=" user input-style" type="password" name="contra2" id="contra2" required placeholder="Confirmar contraseña">
+          </div>
+        </div>
+        
+        <div class="submit">
+          <button type="submit">Actualizar</button>
+        </div>
 
-      <div class="user username">
-        <span class="icon uil uil-user"></span>
-        <input class="user  input-style" type="text" name="nombre" id="" value="<?php echo  $datosDelUsuario->nombre; ?>" required>
-      </div>
-
-      <div class="user password">
-        <span class="icon uil uil-user"></span>
-        <input class=" user input-style" type="text" name="dire" id="" value="<?php echo $datosDelUsuario->apellido;  ?>" required>
-      </div>
-      <div class="user disable username">
-        <span class="icon disable uil uil-envelope-edit"></span>
-        <input class="user disable  input-style" type="email" name="correo" id="" value="<?php echo $datosDelUsuario->email; ?>" required disabled>
-      </div>
-      <INPut name="ftpd" type="hidden" value="<?php echo  $datosDelUsuario->foto ?>"> </INPut>
-      <input name="foto" type="file" accept="image/*" />
-
-      <div class="submit">
-        <button type="submit">Actualizar</button>
-      </div>
-
-    
-
-
-
-     </form>
-  </div>
-
+        <a class="presForm link" href="">¿Olvidaste tu contraseña?</a>
+        <h3 class="presForm" onclick="cambiarForm(2)">Actualizar Perfil</h3>
+      </form>
+    </div>
    
 
     <br><hr><br>
@@ -99,5 +136,6 @@
         </div>
     </div>
 
+    <script type="text/javascript" src="../JavaScript/complementos.js"></script>
 </body>
 </html>
